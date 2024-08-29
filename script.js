16fca45052535028f99f6fe74cae82cd8afd2f00
Daniel Dimas de Souza <101479145+DanielDimas2002@users.noreply.github.com>
2024-08-29 12:09:33 -0300

    Continuação

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const colunas = document.querySelectorAll(".coluna_cards")
 
-const cards = document.querySelectorAll(".cards")
+const cards = document.querySelectorAll(".card")
+
+let draggedCard; 
+
+const dragStart = (event) =>{
+    draggedCard = event.target
+    event.dataTransfer.effectAllowed = "move"
+}
+
+const dragOver = (event) =>{
+    event.preventDefault()
+}
 
 cards.forEach((card) =>{
-    card.addEventListener("dragstart", () =>{
-        
-    })
+    card.addEventListener("dragstart", dragStart)
+})
+
+colunas.forEach((coluna) => {
+    coluna.addEventListener("dragover", dragOver)
+    coluna.addEventListener("dragenter", dragOver)
 })
