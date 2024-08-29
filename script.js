fc31cd8377f0173031227e979c89fe2a95bfcba0
Daniel Dimas de Souza <101479145+DanielDimas2002@users.noreply.github.com>
2024-08-29 11:38:01 -0300

    Inicio

---
+++ script.js
@@ -0,0 +1,9 @@
+const colunas = document.querySelectorAll(".coluna_cards")
+
+const cards = document.querySelectorAll(".cards")
+
+cards.forEach((card) =>{
+    card.addEventListener("dragstart", () =>{
+        
+    })
+})
