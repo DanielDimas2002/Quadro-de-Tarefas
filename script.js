a5731ceef541ac98ac6c728c830d6a92f56dd82b
Daniel Dimas de Souza <101479145+DanielDimas2002@users.noreply.github.com>
2024-08-30 08:50:04 -0300

    Finalização

--- script.js
+++ script.js
@@ -1,34 +1,61 @@
 const colunas = document.querySelectorAll(".coluna_cards")
 
-const cards = document.querySelectorAll(".card")
+let CardArrastado;
 
-let CardArrastado; 
-
-const dragStart = (event) =>{
+const dragStart = (event) => {
     CardArrastado = event.target
     event.dataTransfer.effectAllowed = "move"
 }
 
-const dragOver = (event) =>{
+const dragOver = (event) => {
     event.preventDefault()
 }
 
-const dragEnter = ({target}) =>{
-    if(target.classList.contains("coluna_cards")){
+const dragEnter = ({ target }) => {
+    if (target.classList.contains("coluna_cards")) {
         target.classList.add("coluna_card_movendo")
     }
 }
 
-const dragLeave = ({target}) =>{
-        target.classList.remove("coluna_card_movendo")
+const dragLeave = ({ target }) => {
+    target.classList.remove("coluna_card_movendo")
 }
 
-cards.forEach((card) =>{
+const drop = ({ target }) => {
+    if (target.classList.contains("coluna_cards")) {
+        target.classList.remove("coluna_card_movendo")
+        target.append(CardArrastado)
+    }
+}
+
+const createCard = ({target}) =>{
+    if (target.classList.contains("coluna_cards")) return
+
+    const card = document.createElement("section")
+
+    card.className = "card"
+    card.draggable = "true"
+    card.contentEditable = "true"
+
+    card.addEventListener("focusout", () =>{
+        card.contentEditable = "false"
+
+        if(!card.textContent){
+            card.remove()
+        }
+    })
+
     card.addEventListener("dragstart", dragStart)
-})
+
+    target.append(card)
+
+    card.focus()
+}
 
 colunas.forEach((coluna) => {
     coluna.addEventListener("dragover", dragOver)
     coluna.addEventListener("dragenter", dragEnter)
     coluna.addEventListener("dragleave", dragLeave)
+    coluna.addEventListener("drop", drop)
+    coluna.addEventListener("dblclick", createCard)
 })
